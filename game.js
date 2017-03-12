d12105852cf6b0fbc956c0db887d453d6afd3116
Omsi <omsi2000@gmail.com>
2017-03-11 22:02:45 -0800

    :ok_hand:

---
+++ game.js
@@ -0,0 +1,20 @@
+const $ = document.querySelector.bind(document);
+
+$(".ok").addEventListener("mouseover", (e) => {
+    e.target.setAttribute("src", e.target.attributes.src.value);
+})
+
+$(".enter").addEventListener("click", (e) => {
+    document.body.classList.add("entered");
+    var elem = document.body;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    }
+    e.preventDefault();
+})
