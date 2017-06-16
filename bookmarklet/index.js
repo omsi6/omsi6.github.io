c1c53d60c998f48594a301903970dcb8b2f43262
Oden <hello@odensc.me>
2017-06-15 22:20:05 -0700

    Fix it

--- bookmarklet/index.js
+++ bookmarklet/index.js
@@ -3,10 +3,6 @@ const $ = document.querySelector.bind(document);
 fetch("https://omsi6.github.io/bookmarklet/source.js")
 	.then(res => res.text())
 	.then(src => {
-		const safeSrc = src
-			.replace(/\n/g, ";")
-			.replace(/"/g, "'")
-
 		$("#loading").remove();
-		$("#bookmarklet-button").href = "javascript:" + safeSrc;
+		$("#bookmarklet-button").href = "javascript:" + src;
 	});
