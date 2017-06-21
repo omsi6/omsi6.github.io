11bb1f336e20a681a69bffc1902f97b71ab8b57e
Omsi <omsi2000@gmail.com>
2017-06-21 11:30:56 -0700

    Calculator updates.

---
+++ calculator/index.js
@@ -0,0 +1,8 @@
+function getRates() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+    }
+}
