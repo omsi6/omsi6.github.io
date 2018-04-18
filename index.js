eb742df2708d99b4cdb4179419a64fea760287e9
Omsi <omsi2000@gmail.com>
2018-04-17 20:56:34 -0700

    fix draggable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 document.getElementById('image').draggable = false;
 document.getElementById('image2').draggable = false;
 document.getElementById('image3').draggable = false;
+document.getElementById('image4').draggable = false;
+document.getElementById('image5').draggable = false;
