b13cc612528782aec098da8fe74355a1374ee4c8
Omsi <omsi2000@gmail.com>
2018-06-14 20:28:39 -0700

    add colors to landing page

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ document.getElementById('image2').draggable = false;
 document.getElementById('image3').draggable = false;
 document.getElementById('image4').draggable = false;
 document.getElementById('image5').draggable = false;
+document.getElementById('image6').draggable = false;
