e683de8816a2d9f3f7a45656e893b0354a9d14c7
Omsi <omsi2000@gmail.com>
2018-08-03 00:32:06 -0700

    loops, thanks Stop_Sign ❤

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ document.getElementById('image3').draggable = false;
 document.getElementById('image4').draggable = false;
 document.getElementById('image5').draggable = false;
 document.getElementById('image6').draggable = false;
+document.getElementById('image7').draggable = false;
