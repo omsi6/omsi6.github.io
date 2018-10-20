9b54dc0f697f2532578b754cbcee4ebff6c6ce88
Oden <hello@odensc.me>
2018-10-20 16:32:53 -0700

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('image').draggable = false;
-document.getElementById('image2').draggable = false;
-document.getElementById('image3').draggable = false;
-document.getElementById('image4').draggable = false;
-document.getElementById('image5').draggable = false;
