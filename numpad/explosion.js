534154d9c0e096b36eaacfa225d8a2b71cefc34b
Omsi <omsi2000@gmail.com>
2019-07-29 15:54:09 -0700

    numpad: code cleanup

--- numpad/explosion.js
+++ numpad/explosion.js
@@ -5,143 +5,128 @@ window.mozRequestAnimationFrame ||
 window.webkitRequestAnimationFrame ||
 window.msRequestAnimationFrame;
 
-// Canvas
-const c   = document.querySelector('.canvas');
-const ctx = c.getContext('2d');
+// canvas
+const c = document.querySelector(".canvas");
+const ctx = c.getContext("2d");
 
-// Set full-screen
-c.width  = window.innerWidth;
+// set full-screen
+c.width = window.innerWidth;
 c.height = window.innerHeight;
 
-// Options
+// options
 const particlesPerExplosion = 100;
-const particlesMinSpeed     = 6;
-const particlesMaxSpeed     = 10;
-const particlesMinSize      = 0.5;
-const particlesMaxSize      = 4;
-const explosions            = [];
+const particlesMinSpeed = 6;
+const particlesMaxSpeed = 10;
+const particlesMinSize = 0.5;
+const particlesMaxSize = 4;
+const explosions = [];
 
 let now, delta;
 let then = performance.now();
 
-// Draw
+// draw
 function draw(ts) {
-// Loop
-requestAnimationFrame(draw);
+    // loop
+    requestAnimationFrame(draw);
 
-// Set NOW and DELTA
-now   = ts;
-delta = now - then;
+    // set NOW and DELTA
+    now = ts;
+    delta = now - then;
 
-  // Update THEN
-  then = ts;
+    // update THEN
+    then = ts;
 
-  // Our animation
-  ctx.clearRect(0, 0, c.width, c.height);
-  drawExplosion();
+    // our animation
+    ctx.clearRect(0, 0, c.width, c.height);
+    drawExplosion();
 
 }
 
-// Draw explosion(s)
+// draw explosion(s)
 function drawExplosion() {
+    if (explosions.length === 0) {
+        return;
+    }
+    for (let i = 0; i < explosions.length; i++) {
+        const explosion = explosions[i];
+        const particles = explosion.particles;
 
-if (explosions.length === 0) {
-  return;
-}
+        if (particles.length === 0) {
+            explosions.splice(i, 1);
+            return;
+        }
 
-for (let i = 0; i < explosions.length; i++) {
+        const particlesAfterRemoval = particles.slice();
+        for (let ii = 0; ii < particles.length; ii++) {
 
-  const explosion = explosions[i];
-  const particles = explosion.particles;
+            const particle = particles[ii];
 
-  if (particles.length === 0) {
-	explosions.splice(i, 1);
-	return;
-  }
+            // check particle size
+            // If 0, remove
+            if (particle.size <= 0) {
+                particlesAfterRemoval.splice(ii, 1);
+                continue;
+            }
 
-  const particlesAfterRemoval = particles.slice();
-  for (let ii = 0; ii < particles.length; ii++) {
+            ctx.beginPath();
+            ctx.arc(particle.x, particle.y, particle.size, Math.PI * 2, 0, false);
+            ctx.closePath();
+            ctx.fillStyle = `rgb(${particle.r}, ${particle.g}, ${particle.b})`;
+            ctx.fill();
 
-	const particle = particles[ii];
-
-	// Check particle size
-	// If 0, remove
-	if (particle.size <= 0) {
-	  particlesAfterRemoval.splice(ii, 1);
-	  continue;
-	}
-
-	ctx.beginPath();
-	ctx.arc(particle.x, particle.y, particle.size, Math.PI * 2, 0, false);
-	ctx.closePath();
-	ctx.fillStyle = 'rgb(' + particle.r + ',' + particle.g + ',' + particle.b + ')';
-	ctx.fill();
-
-	// Update
-	particle.x += particle.xv * (delta * 0.0604);
-	particle.y += particle.yv * (delta * 0.0604);
-	particle.size -= .1 * (delta * 0.0604);
-  }
-
-  explosion.particles = particlesAfterRemoval;
-
-}
-
+            // update
+            particle.x += particle.xv * (delta * 0.0604);
+            particle.y += particle.yv * (delta * 0.0604);
+            particle.size -= 0.1 * (delta * 0.0604);
+        }
+        explosion.particles = particlesAfterRemoval;
+    }
 }
 
 function explode(x, y) {
-
-explosions.push(
-  new explosion(x, y)
-);
-
+    explosions.push(
+        new explosion(x, y)
+    );
 }
 
-// Explosion
+// explosion
 function explosion(x, y) {
-
-this.particles = [];
-
-for (let i = 0; i < particlesPerExplosion; i++) {
-  this.particles.push(
-	new particle(x, y)
-  );
+    this.particles = [];
+    for (let i = 0; i < particlesPerExplosion; i++) {
+        this.particles.push(
+            new particle(x, y)
+        );
+    }
 }
 
-}
-
-// Particle
+// particle
 function particle(x, y) {
-this.x    = x;
-this.y    = y;
-this.xv   = randInt(particlesMinSpeed, particlesMaxSpeed, false);
-this.yv   = randInt(particlesMinSpeed, particlesMaxSpeed, false);
-this.size = randInt(particlesMinSize, particlesMaxSize, true);
-this.r    = realRandInt(71, 91);
-this.g    = realRandInt(35, 55);
-this.b    = realRandInt(158, 178);
+    this.x = x;
+    this.y = y;
+    this.xv = randInt(particlesMinSpeed, particlesMaxSpeed, false);
+    this.yv = randInt(particlesMinSpeed, particlesMaxSpeed, false);
+    this.size = randInt(particlesMinSize, particlesMaxSize, true);
+    this.r = realRandInt(71, 91);
+    this.g = realRandInt(35, 55);
+    this.b = realRandInt(158, 178);
 }
 
-// Returns an random integer, positive or negative
+// returns an random integer, positive or negative
 // between the given value
 function randInt(min, max, positive) {
-
-let num;
-if (positive === false) {
-  num = Math.floor(Math.random() * max) - min;
-  num *= Math.floor(Math.random() * 2) === 1 ? 1 : -1;
-} else {
-  num = Math.floor(Math.random() * max) + min;
+    let num;
+    if (positive === false) {
+        num = Math.floor(Math.random() * max) - min;
+        num *= Math.floor(Math.random() * 2) === 1 ? 1 : -1;
+    } else {
+        num = Math.floor(Math.random() * max) + min;
+    }
+    return num;
 }
 
-return num;
-
-}
-
+// the maximum is inclusive and the minimum is inclusive
 function realRandInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 draw();
