56076f19125fad1a1d3af5911130ac1b143b29da
Jeremiah <phase.variance@gmail.com>
2022-06-23 08:39:15 -0400

    Removed all but the loops game

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    document.getElementById(`shoutout${Math.floor(Math.random() * 6 + 1)}`).style.display = "flex";
-};
-
-function disableShoutoutAnim() {
-    const text = document.getElementById("shoutouttext");
-    text.style.animation = "none";
-    text.style.opacity = "0";
-}
