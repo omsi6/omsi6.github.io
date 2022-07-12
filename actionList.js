6626074cdbf0f75040fe542af8c6ac3d0d2b01b3
Jeremiah <phase.variance@gmail.com>
2022-07-12 10:26:46 -0400

    ImbueSoul wasn't resetting training limits

--- actionList.js
+++ actionList.js
@@ -5547,6 +5547,7 @@ Action.ImbueSoul = new MultipartAction("Imbue Soul", {
         }
         buffs["Imbuement"].amt = 0;
         buffs["Imbuement2"].amt = 0;
+        trainingLimits = 0;
         addBuffAmt("Imbuement3", 1);
         view.updateBuffs();
         view.updateStats();
